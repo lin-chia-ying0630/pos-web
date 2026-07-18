64722498a88b571c1b2c296dfb65040492b7588f
linjiaying <s290510790@gmail.com>
2026-07-18 21:53:32 +0800

    fix: group address records by address type

--- src/components/PolicyAddressSummary.vue
+++ src/components/PolicyAddressSummary.vue
@@ -6,26 +6,27 @@
         <h2>保單地址資訊</h2>
       </div>
       <div class="query-columns query-columns-head"><strong>欄位名稱</strong><strong>內容</strong></div>
-      <dl class="data-grid address-grid">
-        <template v-for="address in policyStore.policyDetail.addressList" :key="address.addressType">
-          <div class="wide query-field">
+      <div v-for="address in policyStore.policyDetail.addressList" :key="address.addressType" class="address-record">
+        <h3>地址 {{ address.addressType }}／{{ policyStore.addressTypeLabel(address.addressType) }}</h3>
+        <dl class="data-grid">
+          <div class="query-field">
             <dt>地址類型</dt>
             <dd>{{ policyStore.addressTypeLabel(address.addressType) }}</dd>
           </div>
-          <div class="wide query-field">
+          <div class="query-field">
             <dt>郵遞區號</dt>
             <dd>{{ `${address.zipCode3 ?? ''}${address.zipCode2 ?? ''}` || '-' }}</dd>
           </div>
-          <div class="wide query-field">
+          <div class="query-field">
             <dt>地址</dt>
             <dd>{{ address.fullWidthAddress || '-' }}</dd>
           </div>
-          <div class="wide query-field">
+          <div class="query-field">
             <dt>email／電話／手機</dt>
             <dd>{{ address.halfWidthAddress || '-' }}</dd>
           </div>
-        </template>
-      </dl>
+        </dl>
+      </div>
     </article>
   </section>
 </template>
